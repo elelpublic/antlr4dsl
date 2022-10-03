efe99e7bd13c817ef82d308f0ede6de52bfc2fa4
Lars Löffler <elelpublic@gmail.com>
2022-10-03 10:33:08 +0200

    Compile and run hello world (without antlr).

---
+++ src/main/java/com/infodesire/poc/antlr4dsl/Main.java
@@ -0,0 +1,9 @@
+package com.infodesire.poc.antlr4dsl;
+
+public class Main {
+
+  public static void main( String... args ) {
+    System.out.println( "Hello World! Looking forward to some parsing." );
+  }
+
+}
